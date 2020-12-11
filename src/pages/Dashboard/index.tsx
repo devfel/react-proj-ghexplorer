a0ffc994be2d213d2f6c8172017e9149fe740515
Felizardo <devfelizardo@gmail.com>
2020-12-10 22:51:54 -0300

    using github api to fetch repositories data for our dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,44 +1,62 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import { FiChevronRight } from "react-icons/fi";
+import api from "../../services/api";
+
 import { Title, Form, Repositories } from "./styles";
 
 import logoImg from "../../assets/logo.svg";
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo("");
+  }
+
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
       <Title>Dashboard - Explore GitHub Repositories</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
-        <button type="submit">Pesquisar</button>
+      <Form onSubmit={handleAddRepository}>
+        <input value={newRepo} onChange={(e) => setNewRepo(e.target.value)} placeholder="Type the repository owner/name" />
+        <button type="submit">Search & Add</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img src="https://github.com/devfel.png" alt="Danilo Vieira" />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="test">
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img src="https://github.com/devfel.png" alt="Danilo Vieira" />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-          </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
 
-          <FiChevronRight size={20} />
-        </a>
         <a href="teste">
-          <img src="https://github.com/devfel.png" alt="Danilo Vieira" />
+          <img src="https://github.com/devfel.png" alt="Felizardo" />
           <div>
-            <strong>rocketseat/unform</strong>
-            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+            <strong>userName/repoName</strong>
+            <p>Example Repository Description</p>
           </div>
 
           <FiChevronRight size={20} />
